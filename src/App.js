9fa4b36bbc05637105e998fea32a09dec1445e9a
Usitha5555 <usithaarachchiofficial@gmail.com>
2024-04-04 11:59:56 +0530

    design

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import axios from 'axios';
 
+import Header from './components/Header';
 import './App.css';
 
-const center = [7.762081916489769, 80.65858279000169];
+const center = [7.926252474659472, 80.62546784024994];
 // const positionKandy = [7.344505821309715, 80.62933056448098];
 // const positionColombo = [6.924722191330576, 79.8600721708302];
 
@@ -63,6 +64,9 @@ export default function App() {
   const [geoJSONData, setGeoJSONData] = useState(null);
 
   useEffect(() => {
+
+    
+
     axios.get('https://api.maptiler.com/data/7a2e8f44-16af-429b-b62c-c3332ffba33f/features.json?key=vIkXdioF5LVFcmvw5yTi')
       .then(response => {
         setGeoJSONData(response.data);
@@ -71,13 +75,27 @@ export default function App() {
         console.error('Error fetching GeoJSON data:', error);
       });
 
-    axios.get('http://localhost:3030/weather')
-      .then(response => {
-        setWeatherData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-      });
+    
+
+    
+      
+    //   const socket = new WebSocket('ws://localhost:3031'); // WebSocket server port
+
+    // socket.onmessage = (event) => {
+    //   const data = JSON.parse(event.data);
+    //   setWeatherData(data);
+    // };
+
+    // return () => {
+    //   socket.close();
+    // };
+
+      fetchWeatherData()
+      const interval = setInterval(fetchWeatherData, 120000); 
+      console.log('api call')
+      return () => clearInterval(interval);
+      
+    
   }, []);
 
   const geoJSONStyle = {
@@ -130,6 +148,16 @@ export default function App() {
   //     </div>
   //   );
   // };
+
+  const fetchWeatherData = () => {
+    axios.get('https://weatherapp-backend-nodejs.onrender.com/weather')
+      .then(response => {
+        setWeatherData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+      });
+  };
   const getWeatherIcon = (weatherCondition) => {
     if (weatherCondition === 'sunny') {
       return L.icon({
@@ -156,8 +184,20 @@ export default function App() {
   };
   
   return (
-    <div>
-      <h1>Weather App</h1>
+    
+    <section>
+      <div className="App">
+          <Header/>
+      </div>
+        
+
+        <div className="app-container">
+        <div className='title'>
+          
+        </div>
+        
+         
+      
 
       <MapContainer
         center={center}
@@ -210,38 +250,14 @@ export default function App() {
                   );
                 })}
 
-        {/* <Marker position={positionKandy} icon={customIcon} 
-        eventHandlers={{
-                  mouseover: (e) => {
-                    e.target.openPopup();
-                  },
-                  mouseout: (e) => {
-                    e.target.closePopup();
-                  },
-              }}>
-          <Popup>
-          {generatePopupContent('Kandy', weatherData)}
-          </Popup>
-        </Marker>
-
-        <Marker position={positionColombo} icon={customIcon} 
-        eventHandlers={{
-                  mouseover: (e) => {
-                    e.target.openPopup();
-                  },
-                  mouseout: (e) => {
-                    e.target.closePopup();
-                  },
-              }}>
-          <Popup>
-          {generatePopupContent('Colombo', weatherData)}
-          </Popup>
-        </Marker> */}
+        
 
 
 
       </MapContainer>
     </div>
+    </section>
+    
   );
 }
 
@@ -256,151 +272,3 @@ export default function App() {
 
 
 
-
-
-
-// import React,{ useState, useEffect } from 'react'
-// import {
-//   MapContainer,
-//   TileLayer,
-//   Polygon,
-//   Marker,
-//   Popup
-// } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css'
-// import {statesData} from './data'
-// import L from 'leaflet';
-// import axios from 'axios';
-
-// import './App.css';
-// const center = [7.762081916489769, 80.65858279000169];
-// const markerPosition = [7.344505821309715, 80.62933056448098];
-
-// const customIcon = L.icon({
-//   iconUrl: './sun.png', // URL to your custom marker icon image
-//   iconSize: [70, 70], // Size of the icon in pixels
-//   iconAnchor: [12, 41], // Anchor point of the icon relative to its top left corner
-//   popupAnchor: [1, -34], // Anchor point for popups relative to the icon's center
-   
-// });
-
-
-// export default function App () {
-//   const [weatherData, setWeatherData] = useState([]);
-  
-//   useEffect(() => {
-//     axios.get('http://localhost:3030/weather')
-//       .then(response => {
-//         setWeatherData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching weather data:', error);
-//       });
-//   }, []);
-
-//   // const renderMarkers = () => {
-//   //   return weatherData.map(item => (
-//   //     <Marker key={item._id} position={[item.latitude, item.longitude]}>
-//   //       <Popup>
-//   //         <h3>{item.district}</h3>
-//   //         <p>Temperature: {item.temperature}°C</p>
-//   //         <p>Humidity: {item.humidity}%</p>
-//   //         <p>Pressure: {item.pressure} hPa</p>
-//   //       </Popup>
-//   //     </Marker>
-//   //   ));
-//   // };
-        
-//   return (
-    
-//     <div>
-      
-//       <h1>Weather App</h1>
-      
-//     <MapContainer
-//     center={center}
-//       zoom={8}
-//       style={{width: '50vw',height: '90vh'}}
-//       dragging={false}
-//       scrollWheelZoom={false}
-//       zoomControl={false}
-      
-//     >
-//       <TileLayer
-//       url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=vIkXdioF5LVFcmvw5yTi"
-//       attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-//       />
-        
-//       {
-//         statesData.features.map((states)=> {
-//           const coordinates = states.geometry.coordinates[0].map((item)=> [item[1], item[0]])
-//           // return <Polygon
-//           //   pathOptions={{
-//           //     fillColor: '#FD8D3C',
-//           //     fillOpacity: 0.7,
-//           //     weight: 2,
-//           //     opacity: 1,
-//           //     dashArray: 3,
-//           //     color: 'white',
-//           //     zIndex: 1,
-//           //   }}
-//           //   positions={coordinates}
-//           //   eventHandlers={{
-//           //     mouseover: (e) => {
-//           //       const layer = e.target;
-//           //       layer.setStyle({
-//           //         fillOpacity:0.7,
-//           //         weight: 2,
-//           //         dashArray: "",
-//           //         color: "white",
-//           //         fillColor: 'green'
-//           //       })
-//           //     },
-//           //     mouseout: (e) =>{
-//           //       const layer = e.target;
-//           //         layer.setStyle({
-//           //           fillOpacity:0.7,
-//           //           weight: 2,
-//           //           dashArray: "3",
-//           //           color: "white",
-//           //           fillColor: '#FD8D3C'
-//           //         })
-//           //     },
-//           //     click: (e) => {
-
-//           //     }
-//           //   }}
-//           // />
-//         })
-//       }
-//       <Marker position={markerPosition} icon={customIcon}
-//       eventHandlers={{
-//         mouseover: (e) => {
-//           e.target.openPopup();
-//         },
-//         mouseout: (e) => {
-//           e.target.closePopup();
-//         },
-//     }}
-//       >
-//         <Popup>
-//         <h3>Kandy</h3>
-//     {weatherData.map(item => {
-//       if (item.district === 'Kandy') {
-//         return (
-//           <div key={item._id}>
-//             <p>Temperature: {item.temperature}°C</p>
-//             <p>Humidity: {item.humidity}%</p>
-//             <p>Pressure: {item.pressure} hPa</p>
-//           </div>
-//         );
-//       } else {
-//         return null;
-//       }
-//     })}
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-//     </div>
-//   );
-// }
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter innerWidth h-container">
+        {/* <img src='./03.png' alt='dfw'  width={20}/> */}
+        <h1>Weather App</h1>
+        <div className="flexCenter h-menu">
+            <a href="">Home</a>
+            <a href="">News</a>
+            <a href="">weather</a>
+            <a href="">About</a>
+            <a href="">Contact us</a>
+            <button className='button'> 
+                <a href="">Login</a>
+            </button>
+        </div>
+      </div>
+       
+    </section>
+  )
+}
+
+export default Header
